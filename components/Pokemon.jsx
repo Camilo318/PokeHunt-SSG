e019c3aaf930a498c192ea0143ffc6eaacb11d87
Camilo318 <camiloa0318@gmail.com>
2020-12-03 14:44:14 -0500

    Add getStaticPaths and getStaticProps to [id].jsx file to pre-render paths Add next/link on Pokemon component to get client navigation

--- components/Pokemon.jsx
+++ components/Pokemon.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import style from './styles/Pokemon.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Pokemon = ({ id, children }) => {
     const source = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`
     return (
-        <div className={style.pokemon}>
-            <div className={style.pokemon__img}>
-                <Image
-                src={source}
-                alt={`Picture of ${children}`}
-                width={240}
-                height={240}
-                />
-            </div>
-            <h4>{ children }</h4>
-        </div>
+        <Link href={`/pokemon/${id}`}>
+            <a>
+                <div className={style.pokemon}>
+                    <div className={style.pokemon__img}>
+                        <Image
+                        src={source}
+                        alt={`Picture of ${children}`}
+                        width={240}
+                        height={240}
+                        />
+                    </div>
+                    <h4>{ children }</h4>
+                </div>
+            </a>
+        </Link>
     )
 }
 
--- pages/pokemon/[id].jsx
+++ pages/pokemon/[id].jsx
@@ -1,10 +1,47 @@
 import React from 'react'
+const api = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=841'
 
-const PokemonDetail = () => {
+export async function getStaticPaths() {
+    const res = await fetch(api)
+    const data = await res.json()
+    const { results } = data
+    const pokemons = await Promise.all(results.map(async item => {
+        const res = await fetch(item.url)
+        const data = await res.json()
+        return data
+    }))
+    const paths = pokemons.map(pokemon => (
+        { params: { id: pokemon.id.toString() } } 
+    ))
+
+    return { paths, fallback: true}
+}
+
+export async function getStaticProps({ params }) {
+    const res = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${params.id}/`
+    )
+    const pokemon = await res.json()
+    return {
+        props: {
+            pokemon
+        }
+    }
+
+}
+
+const PokemonDetail = ({ pokemon }) => {
     return (
-        <div>
-            
-        </div>
+        <>
+          <div className="pokemon-container">
+              <div className="pokemon-detail">
+                  <div className="pokemon-detail__image">
+                      <h2>{pokemon?.name}</h2>
+                      <h3>{pokemon?.id}</h3>
+                  </div>
+              </div>
+          </div>
+        </>
     )
 }
 
