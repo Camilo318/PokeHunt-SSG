75f1f314b5d093906808646c7a8cb0f5af2df688
Camilo318 <camiloa0318@gmail.com>
2020-12-03 17:36:06 -0500

    Add Header Refactoring done in Pagination

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import style from './styles/Header.module.scss'
 const Header = () => {
     return (
-        <header>
-            <div>
-                PokeHunt
+        <header className={style.header}>
+            <div className={style.header__container}>
+                <h2>Poke Hunt</h2>
+                <span>
+                    •••
+                </span>
             </div>
         </header>
     )
