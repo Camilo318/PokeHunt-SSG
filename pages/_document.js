1a66676b4df803c7dce7e62504e349f63b6cce91
Camilo318 <camiloa0318@gmail.com>
2020-12-09 21:10:31 -0500

    Add new font-family Add new font-weight and color

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
       <Html>
         <Head >
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet" />
           <title>
             PokeHunt
           </title>
